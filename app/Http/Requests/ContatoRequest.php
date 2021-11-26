824389b06108579cca6b2095e8cf8ecaaa597a86
ThiagolFPereira <thiagolfpereira@gmail.com>
2021-11-26 17:29:42 -0300

    request

---
+++ app/Http/Requests/ContatoRequest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ContatoRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+
+        switch($this->method()) {
+            case "POST":
+                return [
+                    "name" => 'required|max:100',
+                    "mail" => 'required|max:200|unique:contatos',
+                    "address" => 'required',
+                    "number" => 'required',
+                    "neighborhood" => 'required',
+                    "city" => 'required',
+                    "state" => 'required',
+                    "zip" => 'required',
+                    "phone" => 'required|max:15',
+                    "avatar" => 'nullable|sometimes|image|mimes:jpg,jpeg,png,git'
+
+                ];
+                break;
+
+
+                case "PUT":
+                    return [
+                        "name" => 'required|max:100',
+                        "mail" => 'required|max:200|unique:contatos,email'.$this->id,
+                        "address" => 'required',
+                        "number" => 'required',
+                        "neighborhood" => 'required',
+                        "city" => 'required',
+                        "state" => 'required',
+                        "zip" => 'required',
+                        "phone" => 'required|max:15',
+                        "avatar" => 'nullable|sometimes|image|mimes:jpg,jpeg,png,git'
+
+                    ];
+
+                    break;
+
+
+                    default:break;
+        }
+    }
+}
